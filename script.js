0ee60c4670848d87dba808157de9cdf7ea34ec0f
Tejas Kumar BC <bctejaskumar@gmail.com>
2026-03-15 18:18:20 +0530

    Day2:Added subject selection functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Quiz Website Project Started");
+function startQuiz(subject){
+
+document.getElementById("subjectPage").style.display="none";
+document.getElementById("quizPage").style.display="block";
+
+document.getElementById("question").innerText =
+"You selected " + subject + " subject";
+
+}
+
+function nextQuestion(){
+
+alert("Next question will appear here");
+
+}
